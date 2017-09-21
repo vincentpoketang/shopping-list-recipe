34552fdddb9e3a4e73b62f7a58deb12c16c2dd9d
Vincent <vincentpoketang@gmail.com>
2017-09-20 17:52:51 -0700

    added service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,39 @@
+import {Recipe} from './recipe.model';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Pizza',
+      'It\'s just pizza',
+      'https://static.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg',
+      [
+        new Ingredient('Cheese', 10),
+        new Ingredient('Dough', 2),
+        new Ingredient('Sausage', 14)
+      ]
+    ),
+    new Recipe('Hamburger',
+      'I like hamburger',
+      'https://az616578.vo.msecnd.net/files/2016/03/12/6359341695309050372083788656_fast%20food.jpg',
+      [
+        new Ingredient('Bun', 2),
+        new Ingredient('Cheese', 1),
+        new Ingredient('Meat', 1)
+      ]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService){}
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.slService.addIngredients(ingredients);
+  }
+}
